2b694d2c28165564b0b44ac5296e2e22c0b3e412
shmuel stern <shmuelsstern@gmail.com>
2017-10-25 23:10:38 -0400

    Initial commit

---
+++ resources/views/uploadBalancesFile.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts')
+
+@section('title', 'Upload New Balances')
+
+@section('content')
+<!-- The data encoding type, enctype, MUST be specified as below -->
+<div class='container padding-top'>
+    <div class='row'>
+        <div class='col-xs-12'>
+            <form enctype="multipart/form-data" action="/" method="POST">
+                <!-- MAX_FILE_SIZE must precede the file input field -->
+                <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
+                Select the file with new balances(csv) with the following column headings<br><br>
+                
+                <!-- Name of input element determines name in $_FILES array -->
+                <input name="newBalancesfile" type="file" /><hr>
+                <input type="submit" value="Upload New Balance File" />
+
+            </form>
+        </div>
+    </div>
+</div>
+
+@endsection
