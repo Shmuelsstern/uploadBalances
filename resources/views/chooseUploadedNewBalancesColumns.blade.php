c205488fe98b1b0315da5e2943e99b0b65ec2529
shmuel stern <shmuelsstern@gmail.com>
2017-11-19 18:32:53 -0500

    skipped in between rows

--- resources/views/chooseUploadedNewBalancesColumns.blade.php
+++ resources/views/chooseUploadedNewBalancesColumns.blade.php
@@ -9,37 +9,43 @@
         <div class="xs-col-12">
             <table class="table">
             {!! Form::open(['url' => '/setColumns']) !!} 
-                    
-@foreach($uploadedFile->getParsedFileArray() as $parsedBalance)    
-    
-    @if($loop->first)
-                <thead>
-                    <tr><th>{!! Form::submit('confirmed columns',['id' => 'submitButton']) !!}</th></tr>
-                    <tr>
-    @for($i=0;$i<count($parsedBalance);$i++)
-                        <th>
-    {!! Form::select('column'.$i , ParsedFile::$COLUMNS_TO_CHOOSE['newBalances'], null, ['placeholder' => 'do not import','data-column'=> $i]) !!}
-                        </th>
-    @endfor
-                    </tr>
-                    <tr id='DOSrow' >
-    @for($i=0;$i<count($parsedBalance);$i++)
-                        <th>
-    {!! Form::date('DOS'.$i, null,['style'=>'visibility:hidden','id'=>'DOS'.$i]) !!}
-                        </th>
-    @endfor                    
-                    </tr>
-    @elseif($loop->iteration==2)  
-                <tbody>
-    @endif                
-                    <tr>
+@php 
+$count=0; 
+@endphp  
+
+@foreach($uploadedFile->getParsedFileArray() as $parsedBalance)
+    @if(($count++<5)||($count>($loop->count - 5)))
+        @if($loop->first)
+                    <thead>
+                        <tr>    
+                            <th>{!! Form::submit('confirmed columns',['id' => 'submitButton']) !!}</th>
+                            <th>There are {{$loop->count}} rows</th>
+                        </tr>
+                        <tr>
+            @for($i=0;$i<count($parsedBalance);$i++)
+                            <th>
+            {!! Form::select('column'.$i , ParsedFile::$COLUMNS_TO_CHOOSE['newBalances'], null, ['placeholder' => 'do not import','data-column'=> $i]) !!}
+                            </th>
+            @endfor
+                        </tr>
+                        <tr id='DOSrow' >
+            @for($i=0;$i<count($parsedBalance);$i++)
+                            <th>
+            {!! Form::date('DOS'.$i, null,['style'=>'visibility:hidden','id'=>'DOS'.$i]) !!}
+                            </th>
+            @endfor                    
+                        </tr>
+        @elseif($loop->iteration==2)  
+                    <tbody>
+        @endif                
+                        <tr>
         @foreach($parsedBalance as $data)
             @if($loop->parent->first)
                         <th>
             @else  
                         <td>
             @endif
-        {{ $data }}
+            {{ $data }}
             @if($loop->parent->first)
                         </th>
             @else  
@@ -47,14 +53,15 @@
             @endif
 
         @endforeach
-                    </tr>
-    @if($loop->first)
+                        </tr>
+        @if($loop->first)
                 </thead>
-    @elseif($loop->last)  
+        @elseif($loop->last)  
                 </tbody>
-    @endif  
+        @endif
+    @endif
 
-    @endforeach
+@endforeach
             {!! Form::close() !!}
             </table>
         </div>
